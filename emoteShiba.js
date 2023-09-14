1747ee803719c2f67ffee9b4277570b963914d86
BookmDan <danoh329@gmail.com>
2023-09-14 04:47:16 -0700

    for some reason not displaying images

--- emoteShiba.js
+++ emoteShiba.js
@@ -1,29 +1,128 @@
-const apiUrl = 'http://localhost:3000/shibas'; 
+// const apiUrl = 'http://localhost:3000/shibas'; 
 
-let shibaImages = []; // Store the fetched images here
+// let shibaImages = []; // Store the fetched images here
+
+// const imageContainer = document.getElementById('imageContainer');
+// const searchInput = document.getElementById('searchInput');
+// const searchButton = document.getElementById('searchButton');
+
+// function fetchImages() {
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const shibaImages = data;
+
+//       // Split the array into rows of 3 for the 3x3 grid
+//       const rows = splitByRow(shibaImages, 3);
+
+//       // Display the images in a 3x3 grid
+//       displayImageGrid(rows);
+//     })
+//     .catch((error) => {
+//       console.error('Error fetching images:', error);
+//     });
+// }
+
+// function splitByRow(array, rowSize) {
+//   const splitByRow = [];
+//   for (let i = 0; i < array.length; i += rowSize) {
+//     splitByRow.push(array.slice(i, i + rowSize));
+//   }
+//   return chunkedArray;
+// }
+
+// function displayImageGrid(rows) {
+//   imageContainer.innerHTML = '';
+
+//   rows.forEach((row) => {
+//     const rowDiv = document.createElement('div');
+//     rowDiv.className = 'image-row';
+
+//     row.forEach((imageData) => {
+//       //destructuring
+//       const { id, imageUrl, name, tag } = imageData;
+//       const imageWrapper = document.createElement('div');
+//       imageWrapper.className = 'image-wrapper';
+
+//       const image = document.createElement('img');
+//       image.src = imageUrl;
+//       image.alt = `Image ${id} - ${name}`;
+//       image.title = `Tag: ${tag}`;
+
+//       const tagOverlay = document.createElement('div');
+//       tagOverlay.className = 'tag-overlay';
+//       tagOverlay.textContent = tag; 
+
+      image.addEventListener('mouseenter', () => {
+        image.classList.add('highlighted'); 
+      });
+
+      image.addEventListener('mouseleave', () => {
+        image.classList.remove('highlighted'); 
+      });
+
+
+//       imageWrapper.appendChild(image);
+//       imageWrapper.appendChild(tagOverlay); 
+//       rowDiv.appendChild(imageWrapper);
+//     });
+
+//     imageContainer.appendChild(rowDiv);
+//   });
+// }
+
+// function searchForTag(keyword) {
+//   const matchedImages = shibaImages.filter((image) =>
+//     image.tag.toLowerCase() === keyword.toLowerCase()
+//   );
+
+//   if (matchedImages.length > 0) {
+//     // Display only the matched images and highlight them
+//     displayImages(matchedImages);
+//   } else {
+//     // If no matches found, display all images without highlighting
+//     displayImages(shibaImages);
+//   }
+// }
+
+// searchInput.addEventListener('keydown', event => {
+//   if (event.key === 'Enter') {
+//     const userInput = searchInput.value.trim();
+//     if (userInput) {
+//       searchForTag(userInput);
+//     }
+//   }
+// });
+
+// searchButton.addEventListener('click', () => {
+//   const keyword = searchInput.value.trim();
+//   if (keyword) {
+//     searchForTag(keyword);
+//   }
+// });
+
+// // Call the fetchImages function to load and display the images
+// fetchImages();
+const apiUrl = 'http://localhost:3000/shibas';
 
 const imageContainer = document.getElementById('imageContainer');
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
 
 function fetchImages() {
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      const shibaImages = data;
-
       // Split the array into rows of 3 for the 3x3 grid
-      const rows = chunkArray(shibaImages, 3);
+      const rows = splitArray(data, 3);
 
       // Display the images in a 3x3 grid
-      displayImageGrid(rows);
+      displayImages(rows);
     })
     .catch((error) => {
       console.error('Error fetching images:', error);
     });
 }
 
-function chunkArray(array, chunkSize) {
+function splitArray(array, chunkSize) {
   const chunkedArray = [];
   for (let i = 0; i < array.length; i += chunkSize) {
     chunkedArray.push(array.slice(i, i + chunkSize));
@@ -31,7 +130,7 @@ function chunkArray(array, chunkSize) {
   return chunkedArray;
 }
 
-function displayImageGrid(rows) {
+function displayImages(rows) {
   imageContainer.innerHTML = '';
 
   rows.forEach((row) => {
@@ -48,21 +147,7 @@ function displayImageGrid(rows) {
       image.alt = `Image ${id} - ${name}`;
       image.title = `Tag: ${tag}`;
 
-      const tagOverlay = document.createElement('div');
-      tagOverlay.className = 'tag-overlay';
-      tagOverlay.textContent = tag; 
-
-      image.addEventListener('mouseenter', () => {
-        image.classList.add('highlighted'); 
-      });
-
-      image.addEventListener('mouseleave', () => {
-        image.classList.remove('highlighted'); 
-      });
-
-
       imageWrapper.appendChild(image);
-      imageWrapper.appendChild(tagOverlay); 
       rowDiv.appendChild(imageWrapper);
     });
 
@@ -70,35 +155,6 @@ function displayImageGrid(rows) {
   });
 }
 
-function searchForTag(keyword) {
-  const matchedImages = shibaImages.filter((image) =>
-    image.tag.toLowerCase() === keyword.toLowerCase()
-  );
-
-  if (matchedImages.length > 0) {
-    // Display only the matched images and highlight them
-    displayImages(matchedImages);
-  } else {
-    // If no matches found, display all images without highlighting
-    displayImages(shibaImages);
-  }
-}
-
-searchInput.addEventListener('keydown', event => {
-  if (event.key === 'Enter') {
-    const userInput = searchInput.value.trim();
-    if (userInput) {
-      searchForTag(userInput);
-    }
-  }
-});
-
-searchButton.addEventListener('click', () => {
-  const keyword = searchInput.value.trim();
-  if (keyword) {
-    searchForTag(keyword);
-  }
-});
-
 // Call the fetchImages function to load and display the images
 fetchImages();
+
